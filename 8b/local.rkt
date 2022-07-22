48b2327e56d1ece643b5ebc467f15cba4745a098
Electricpower7804 <107375098+Electricpower7804@users.noreply.github.com>
2022-07-21 23:41:17 -0700

    Add files via upload

---
+++ 8b/local.rkt
@@ -0,0 +1,18 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname local) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+;; To form a local expression:
+
+;(local [<definition> ...] <---- definitions
+;  <expression>)           <---- body
+
+(local [(define a 1)  ;definitions
+        (define b 2)]
+
+  (+ a b))            ;body
+
+(local [(define p "accio ")
+        (define (fetch n) (string-append p n))]
+
+  (fetch "portkey"))
